f48c9bdee753a4f9f76e432db6632d51b4644005
LucasLosano <losano3@hotmail.com>
2021-06-29 15:48:41 -0300

    Desafio4

---
+++ Desafio4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int valor = random.Next(0,11);
+            int palpite;
+            while(true)
+            {
+                Console.Write("Digite um número de 0 a 10: ");
+                palpite = int.Parse(Console.ReadLine());
+
+                if(palpite == valor)
+                    break;
+
+                Console.WriteLine("Você errou ... Tente novamente.");
+            }
+
+            Console.WriteLine("Parabéns, você acertou!");
+        }
+    }
+}
